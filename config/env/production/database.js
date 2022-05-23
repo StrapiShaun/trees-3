6f5d410c9a073d2a65daff6c45a7507943149ba1
Shaun Brown <shaun.brown@strapi.io>
2022-05-23 16:50:16 +0200

    correct spelling error

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,11 +9,11 @@ module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST'), 
-        port: env.int('DATABASE_PORT'), 
-        database: env('DATABASE_NAME'), 
-        user: env('DATABASE_USERNAME'), 
-        password: env('DATABASE_PASSWORD'),
+        host,
+        port,
+        database,
+        user,
+        password,
         ssl: {
           rejectUnauthorized: false,
         },
@@ -22,36 +22,4 @@ module.exports = ({ env }) => ({
     },
   });
 
- /*const parse = require('pg-connection-string').parse;
-  const config = parse(process.env.DATABASE_URL);
-  
-  module.exports = () => ({
-    connection: {
-      client: 'postgres',
-      connection: {
-        ...config,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-    },
-  });
 
-/*  const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
-module.exports = () => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-  },
-}); */
