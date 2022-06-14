3ebc5f8c71097d859f8b78856aad77e97104080e
DMehaffy <derrickmehaffy@gmail.com>
2022-06-14 15:29:51 +0100

    Update database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,15 +1,7 @@
-console.log(process.env.DATABASE_URL)
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-//const parse = require('pg-connection-string').parse;
-//const config = parse(process.env.DATABASE_URL);
-            
-let config = {
-  host: "blah",
-  port: 5432,
-  database: "blah",
-  user: "blah",
-   password: "blah",
-}
+console.log(config);
 
 module.exports = ({ env }) => ({
   connection: {
