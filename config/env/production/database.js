0466c1d78e0b9b8507a39bf46a8ec723662e363a
Shaun Brown <shaun.brown@strapi.io>
2022-06-14 14:11:55 +0200

    db config update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,23 +1,43 @@
+// const parse = require("pg-connection-string").parse;
+
+// const config = parse(process.env.DATABASE_URL);
+
+
+// module.exports = ({ env }) => ({
+//     connection: {
+//       client: 'postgres',
+//       connection: {
+//         host: config.host,
+//         port: config.port,
+//         database: config.database,
+//         user: config.user,
+//         password: config.password,
+//         ssl: {
+//           rejectUnauthorized: false,
+//         },
+//       },  
+//       debug: false,
+//     },
+//   });
 const parse = require("pg-connection-string").parse;
 
-const config = parse(process.env.DATABASE_URL);
-
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
 
 module.exports = ({ env }) => ({
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        user: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },  
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
+      },
       debug: false,
-    },
-  });
+  },
+});
 
 
